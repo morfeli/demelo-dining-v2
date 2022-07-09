1da103a0a22fa9a98a986360b2a29ea8314aa821
morfeli <fmoreira95@hotmail.com>
2022-07-09 12:52:49 -0400

    demelo dining redo

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -12,16 +12,16 @@ export const Hero: React.FC = ({}): JSX.Element => {
       id="Hero"
       className="flex flex-col px-4 py-8 md:justify-evenly font-Bai"
     >
-      <div className="flex flex-col md:flex-row md:justify-around">
-        <div className="flex flex-col items-baseline justify-between md:h-40 md:mt-10">
-          <h1 className="text-2xl md:text-3xl">Demelo Dining</h1>
-          <p className="pt-4 text-md md:text-xl">
+      <div className="flex flex-col sm:flex-row sm:justify-around sm:items-baseline">
+        <div className="flex flex-col items-baseline md:mt-10">
+          <h1 className="py-4 text-2xl md:text-3xl">Demelo Dining</h1>
+          <p className="py-4 text-md md:text-xl">
             Private Dining and Catering Hospitality Group
           </p>
           <p className="py-4 md:text-lg">
             Redefining the private dining experience
           </p>
-          <button className="px-4 py-2 font-bold bg-white rounded-full shadow-xl cursor-pointer">
+          <button className="px-4 py-2 font-bold bg-white rounded-full shadow-xl cursor-pointer md:px-8 md:py-4">
             Contact
           </button>
         </div>
